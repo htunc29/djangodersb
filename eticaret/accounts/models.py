c2fabcdde779fca461c467039855e318260718bc
Hüseyin Tunç <huseyint428@gmail.com>
2025-11-28 11:24:01 +0300

    Add Category and Product models, migrations, and admin registration; update views for home page

--- eticaret/accounts/models.py
+++ eticaret/accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category_name=models.CharField(max_length=50)
+    slug=models.SlugField(unique=True,blank=False)
+
+    def __str__(self):
+        return self.category_name
+
+class Product(models.Model):
+    product_name=models.CharField(max_length=100)
+    description=models.TextField(max_length=500)
+    amount=models.DecimalField(max_digits=10,decimal_places=2)
+    slug=models.SlugField(unique=True,blank=False)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE,related_name="products")
+    def __str__(self):
+        return self.product_name
+
